c91a5c5d6d89250d29cbbae62f48e8c10b4740e8
mykhayloserdyuk <mischaserdyuk@gmail.com>
2025-05-09 00:11:31 +0200

    temp

--- api.py
+++ api.py
@@ -22,7 +22,7 @@ class OpenAIClient:
         response = self.client.chat.completions.create(
             messages=messages,
             model="gpt-3.5-turbo",
-            temperature=0.7
+            temperature=0
         )
 
         result = response.choices[0].message.content
@@ -61,7 +61,7 @@ class OpenAIClient:
         response = self.client.chat.completions.create(
             messages=conversation,
             model="gpt-3.5-turbo",
-            temperature=0.7,
+            temperature=0,
             response_format={"type": "json_object"}
         )
         result = response.choices[0].message.content
@@ -83,7 +83,7 @@ class OpenAIClient:
         response = self.client.chat.completions.create(
             messages=messages,
             model="gpt-3.5-turbo",
-            temperature=0.7
+            temperature=0
         )
         result = response.choices[0].message.content
         print("Summarize Context:", result)
@@ -110,7 +110,7 @@ class DeepSeekClient:
         response = self.client.chat.completions.create(
             messages=messages,
             model="deepseek-chat",
-            temperature=0.7
+            temperature=0
         )
 
         result = response.choices[0].message.content
